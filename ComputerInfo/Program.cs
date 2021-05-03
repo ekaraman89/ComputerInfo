5e5f554df1acbceb3a7eaf11d55dfbfa81b16b73
emrah karaman <ekaraman89@hotmail.com>
2021-05-03 08:39:44 +0300

    Add project files.

---
+++ ComputerInfo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ComputerInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
