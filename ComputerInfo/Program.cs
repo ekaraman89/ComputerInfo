42663d62a7fc2a432032a4e7c6746b8789bfaa58
emrah karaman <ekaraman89@hotmail.com>
2021-05-03 08:42:37 +0300

    cihaz bilgileri cekildi

--- ComputerInfo/Program.cs
+++ ComputerInfo/Program.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using DeviceId;
+using DeviceId.Encoders;
+using DeviceId.Formatters;
+using System;
 
 namespace ComputerInfo
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var builder = new DeviceIdBuilder();
+            builder.Formatter = new StringDeviceIdFormatter(new PlainTextDeviceIdComponentEncoder());
+            var machineName = builder.AddProcessorId().ToString();
+            var cpu = builder.AddMachineName().ToString();
+            var motherCard = builder.AddMotherboardSerialNumber().ToString();
+            var osInstallation = builder.AddOSInstallationID().ToString();
+            var systemDrive = builder.AddSystemDriveSerialNumber().ToString();
+
+            Console.WriteLine($"----------------------------------------\n" +
+                $"machine name: {machineName}\n" +
+                $"cpu name: {cpu}\n" +
+                $"motherCard name: {motherCard}\n" +
+                $"osInstallation name: {osInstallation}\n" +
+                $"systemDrive name: {systemDrive}\n" +
+                $"----------------------------------------");
+
+            Console.ReadLine();
         }
     }
 }
